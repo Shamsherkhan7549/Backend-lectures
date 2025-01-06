16695d1f1c10218be24c425f93fde5ead6333a2c
Shamsher Khan <168928475+Shamsherkhan7549@users.noreply.github.com>
2025-01-06 19:42:31 +0530

    Update userRoute.js

--- ROUTER/routers/userRoute.js
+++ ROUTER/routers/userRoute.js
@@ -12,7 +12,7 @@ router.get('/:id', (req, res)=>{
     res.send(`specific user page based on id`)
 });
 
-router.post('', (req, res)=>{
+router.post('/', (req, res)=>{
     res.send('Insert new user in DB')
 });
 
