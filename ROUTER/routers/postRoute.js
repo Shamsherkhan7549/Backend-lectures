3a9f979fc33cc9c5da8cad087979fa9ae3709721
Shamsher Khan <168928475+Shamsherkhan7549@users.noreply.github.com>
2025-01-06 19:43:48 +0530

    Update postRoute.js

--- ROUTER/routers/postRoute.js
+++ ROUTER/routers/postRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //post
-router.get('', (req, res)=>{
+router.get('/', (req, res)=>{
     res.send('post home page')
 });
 
@@ -10,7 +10,7 @@ router.get('/:id', (req, res)=>{
     res.send(`specific post page based on id`)
 });
 
-router.get('/', (req, res)=>{
+router.post('/', (req, res)=>{
     res.send('Insert new post in DB')
 
 });
